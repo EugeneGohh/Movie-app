a8b944ba29bdfe988a2d440d73f9f2efd5ffd2da
Eugene Goh <gzkgt11@gmail.com>
2021-04-03 00:20:49 +0800

    Added API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./Body.css"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Body.css";
+
+const API = "0e94f803d16ffacb3a17c146abab30a2";
+const URL = "https://api.themoviedb.org/3";
+
+const reqs = {
+  fetchTrending: `${URL}/trending/movie/week?api_key=${API}`,
+};
 
 function Body() {
-    return (
-        <div className="body">
-            
-        </div>
-    )
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(reqs.fetchTrending)
+      .then((res) => {
+        setMovies(res.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="body">
+      {movies.map((movie, i) => {
+        return <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="This is a movie" key={i}/>;
+      })}
+    </div>
+  );
 }
 
-export default Body
+export default Body;
--- src/components/Request.js
+++
@@ -1,8 +0,0 @@
-const API = "0e94f803d16ffacb3a17c146abab30a20";
-const URL = "https://api.themoviedb.org/3"
-
-const reqs = {
-    fetchTrending: `${URL}/trending/movie/week?api_key=${API}`
-}
-
-export default reqs;
