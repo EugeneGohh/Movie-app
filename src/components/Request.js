a438668a73a4b4cbbf78b32421968a03e2b29833
Eugene Goh <gzkgt11@gmail.com>
2021-04-02 23:30:42 +0800

    fetch trending movie

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,5 +1,8 @@
 const API = "0e94f803d16ffacb3a17c146abab30a20";
+const URL = "https://api.themoviedb.org/3"
 
-const req = {
-    
+const reqs = {
+    fetchTrending: `${URL}/trending/movie/week?api_key=${API}`
 }
+
+export default reqs;
