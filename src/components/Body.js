5567b9adccefc344bca9b2e115065c30d2afd9e2
Eugene Goh <gzkgt11@gmail.com>
2021-04-03 08:36:28 +0800

    Added grid and prevent zoom when inspect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
+import Reqs from "./Request";
 import axios from "axios";
 import "./Body.css";
-
-const API = "0e94f803d16ffacb3a17c146abab30a2";
-const URL = "https://api.themoviedb.org/3";
-
-const reqs = {
-  fetchTrending: `${URL}/trending/movie/week?api_key=${API}`,
-};
 
 function Body() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     axios
-      .get(reqs.fetchTrending)
+      .get(Reqs.fetchTrending)
       .then((res) => {
         setMovies(res.data.results);
       })
@@ -22,10 +16,11 @@ function Body() {
   }, []);
 
   return (
-    <div className="body">
-      {movies.map((movie, i) => {
-        return <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="This is a movie" key={i}/>;
-      })}
+    <div className="movie_container">
+    {movies.map((movie, i) => {
+      return (
+      <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="This is a movie" key={i} className="movie_here"/>
+    )})}
     </div>
   );
 }
---
+++ src/components/Request.js
@@ -0,0 +1,8 @@
+const API = "0e94f803d16ffacb3a17c146abab30a2";
+const URL = "https://api.themoviedb.org/3";
+
+const Reqs = {
+  fetchTrending: `${URL}/trending/movie/week?api_key=${API}`,
+};
+
+export default Reqs;
